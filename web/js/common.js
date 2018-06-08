4b3092d604c41b218cba5066e6512ee3f1776abb
dmitriy <dd@linkprofit.com>
2018-06-08 11:58:41 +0300

    add plagin magnific popup

--- web/js/common.js
+++ web/js/common.js
@@ -86,4 +86,30 @@ $(function() {
         }
     });
 
+    $('.header__center-button-wrap a').on('click', function(e){
+       e.preventDefault();
+    });
+
+    $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+
 });
