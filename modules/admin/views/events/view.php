e12d20bd7163f7d220cdbd7e32a443dfcf727bdf
tweeker88 <M1995991m>
2018-06-13 06:29:32 +0300

    delete button 'delete' to event

--- modules/admin/views/events/view.php
+++ modules/admin/views/events/view.php
@@ -16,13 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Обновить', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
         <?= Html::a('Загрузить изображение', ['set-image', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
 
